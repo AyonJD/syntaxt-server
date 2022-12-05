ef0eb2184bc1638ea9fb6e518c222ed72a37d4a1
Ayon Jodder <ayonjodder177@gmail.com>
2022-12-05 23:17:42 +0600

    user model

---
+++ Models/UserModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLenght: 6,
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"]
+    },
+}, { timestamps: true });
+
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;
