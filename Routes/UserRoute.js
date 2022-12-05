1a26bbf566c67a3dd11552fde0e072ba02f1af08
Ayon Jodder <ayonjodder177@gmail.com>
2022-12-05 23:15:34 +0600

    User route

---
+++ Routes/UserRoute.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/', (req, res) => { });
+router.post('/login', (req, res) => { });
+router.get('/:id', (req, res) => { });
+
+module.exports = router;
