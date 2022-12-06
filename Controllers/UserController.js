c9ab7d0ca7b7655009f75abb8f94765806e18c08
Ayon Jodder <ayonjodder177@gmail.com>
2022-12-06 10:38:48 +0600

    Update user role

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -72,3 +72,14 @@ module.exports.getSingleUser = async (req, res) => {
         res.status(500).json({ success: false, message: "Internal server error", error: err.message });
     }
 }
+
+module.exports.updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { role } = req.body;
+    try {
+        const result = await UserModel.findByIdAndUpdate(id, { role }, { new: true });
+        res.status(200).json({ success: true, message: "Successfully updated user", result });
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Internal server error", error: err.message });
+    }
+}
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { createUser, getUser, loginUser, getSingleUser } = require('../Controllers/UserController.js');
+const { createUser, getUser, loginUser, getSingleUser, updateUser } = require('../Controllers/UserController.js');
 const router = express.Router();
 
 router.get('/', getUser);
 router.post('/signup', createUser)
 router.post('/login', loginUser)
 router.get('/:id', getSingleUser)
+router.post('/update/:id', updateUser)
 
 module.exports = router;
